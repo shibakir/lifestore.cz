d0d41181c4ed02b43113486cdab56476cf8a533c
Kirill Shibanov <shibakir@fit.cvut.cz>
2025-01-30 12:08:51 +0100

    add default font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -29,6 +29,11 @@ export default {
         secondary: "var(--color-text-secondary)",
       }
     },
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
