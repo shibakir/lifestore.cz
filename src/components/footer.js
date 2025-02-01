5981b8a64bdf5f51ce3c969d542b46f38604842f
Kirill Shibanov <shibakir@fit.cvut.cz>
2025-02-01 19:50:15 +0100

    update styles footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,27 +13,27 @@ export default function Footer() {
     return (
         <footer className="bg-background-tertiary pt-10 lg:pt-20 pb-10 lg:pb-20 rounded-tl-[5vw] rounded-tr-[5vw]">
             <div className="main flex flex-col-reverse lg:flex-row">
-                <div className="w-full lg:w-1/3">
-                    <div className="min-h-full grid grid-cols-1 grid-rows-3 gap-4">
-                        <div>
+                <div className="w-full xl:w-1/3">
+                    <div className="min-h-full grid grid-cols-1 lg:grid-rows-3 gap-4">
+                        <div className="w-full lg:w-3/4">
                             <Title
-                                className="mb-3 text-text-primary"
+                                className="xl:min-w-[700px] mb-3 text-text-primary hidden lg:block"
                                 title={"Začněte plánovat svou akci s námi!"}
                             />
                             <Text
-                                className="text-text-primary"
+                                className="xl:min-w-500px] text-text-primary"
                                 text={"Neváhejte nás kontaktovat! Pomůžeme vám s plánováním a" +
                                 " zajištěním profesionálního týmu pro vaši akci. Vyplněním" +
                                 " formuláře se dostanete o krok blíže k úspěchu."}
                             />
                         </div>
                         <div>
-                            <div className="space-y-2 lg:space-y-4">
-                                <div className="space-y-2  2xl:space-x-2">
+                            <div className="space-y-2">
+                                <div className="space-y-2">
                                     <Button
                                         text="+420 773 530 000"
                                         link="tel:+420773530000"
-                                        className="mr-2 bg-button-button-2 hover:bg-button-button-3 text-text-secondary text-base xl:text-lg p-6"
+                                        className="mr-5 bg-button-button-2 hover:bg-button-button-3 text-text-secondary text-base xl:text-lg p-6"
                                     />
                                     <Button
                                         text="marika.pusova@lifestore.cz"
@@ -41,11 +41,11 @@ export default function Footer() {
                                         className="bg-button-button-2 hover:bg-button-button-3 text-text-secondary text-base xl:text-lg p-6"
                                     />
                                 </div>
-                                <div className="space-x-2 2xl:space-x-5">
+                                <div>
                                     <Button
                                         text="lifestore.cz"
                                         link="https://www.lifestore.cz/"
-                                        className="bg-button-button-3 hover:bg-button-button-3 text-text-primary text-base xl:text-lg p-6"
+                                        className="mr-5 mb-2 bg-button-button-3 hover:bg-button-button-3 text-text-primary text-base xl:text-lg p-6"
                                         icon={faInstagram}
                                     />
                                     <Button
@@ -57,7 +57,7 @@ export default function Footer() {
                                 </div>
                             </div>
                         </div>
-                        <div className="flex items-center lg:items-end">
+                        <div className="flex flex-nowrap md:items-start lg:items-center 2xl:items-end">
                             <div className="mt-5 lg:mt-0 flex flex-row items-end justify-between w-full">
                                 <Link href="/">
                                     <Image src="/globe.svg" alt="Logo" width={100} height={100}/>
@@ -66,9 +66,9 @@ export default function Footer() {
                                     className="text-text-primary text-[10px] lg:text-sm"
                                     text={
                                         "LifeStore s.r.o.\n"
-                                        +"IČO: 24123820\n"
-                                        +"Jana Přibíka 953/15, 190 00 Praha\n"
-                                        +"Všechna práva vyhrazena © 2025"
+                                        + "IČO: 24123820\n"
+                                        + "Jana Přibíka 953/15, 190 00 Praha\n"
+                                        + "Všechna práva vyhrazena © 2025"
                                     }
                                 />
                             </div>
@@ -76,7 +76,7 @@ export default function Footer() {
                     </div>
                 </div>
                 <div className="hidden lg:block lg:w-1/3"></div>
-                <div className="mb-10 lg:mb-0 w-full lg:w-1/3">
+                <div className="mb-10 lg:mb-0 w-full xl:w-1/3 flex flex-col items-center">
                     <FooterForm/>
                 </div>
             </div>
