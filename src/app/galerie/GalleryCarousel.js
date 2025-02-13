2d8bf396a345720880694167804024547844c2aa
Kirill Shibanov <shibakir@fit.cvut.cz>
2025-02-13 16:50:02 +0100

    replace popup gallery carousel

--- src/app/galerie/GalleryCarousel.js
+++ src/app/galerie/GalleryCarousel.js
@@ -1,96 +1,9 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { X, ChevronLeft, ChevronRight } from "lucide-react";
+import { useState } from "react";
 import {Button as ShadCNButton} from "@/components/ui/button";
+import GalleryCarousel from "@/common/GalleryCarousel/GalleryCarousel";
 
-function GalleryCarousel({ images, initialIndex = 0, isCarouselOpen, setIsCarouselOpen }) {
-    const [isOpen, setIsOpen] = useState(isCarouselOpen);
-    const [currentIndex, setCurrentIndex] = useState(initialIndex);
-    const [fade, setFade] = useState(false);
-
-    useEffect(() => {
-        setIsOpen(isCarouselOpen);
-    }, [isCarouselOpen]);
-
-    useEffect(() => {
-        if (isOpen) {
-            setCurrentIndex(initialIndex);
-        }
-    }, [isOpen, initialIndex]);
-
-    const closeCarousel = () => {
-        setIsOpen(false);
-        setIsCarouselOpen(false);
-    };
-
-    const nextImage = () => {
-        setFade(true);
-        setTimeout(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-            setFade(false);
-        }, 300);
-    };
-
-    const prevImage = () => {
-        setFade(true);
-        setTimeout(() => {
-            setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-            setFade(false);
-        }, 300);
-    };
-
-    return (
-        <div className="relative">
-            {isOpen && (
-                <div
-                    className="fixed inset-0 w-full h-full bg-gray-900 bg-opacity-50 backdrop-blur-xl flex items-center justify-center z-50"
-                    onClick={closeCarousel}
-                >
-                    <div
-                        className="w-[90%] h-[80%] lg:w-[80%] lg:h-[80%] flex flex-col items-center justify-center relative p-4"
-                        onClick={(e) => e.stopPropagation()}
-                    >
-                        <button onClick={closeCarousel} className="absolute top-3 right-3 text-text-primary">
-                            <X size={40}/>
-                        </button>
-
-                        <button onClick={prevImage}
-                                className="absolute left-3 top-1/2 transform -translate-y-1/2 text-text-primary lg:block hidden">
-                            <ChevronLeft size={70}/>
-                        </button>
-
-                        <div
-                            className="flex items-center justify-center max-w-[100%] lg:max-w-[95%] max-h-full overflow-hidden">
-                            <img
-                                key={images[currentIndex].id}
-                                src={images[currentIndex].image}
-                                alt="Gallery photo"
-                                className={`w-full h-full object-cover transition-opacity duration-500 ease-in-out cursor-pointer ${fade ? "opacity-0" : "opacity-100"}`}
-                                loading="lazy"
-                            />
-                        </div>
-
-                        <button onClick={nextImage}
-                                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-text-primary lg:block hidden">
-                            <ChevronRight size={70}/>
-                        </button>
-
-                        <div className="lg:hidden absolute bottom-3 left-0 w-full flex justify-center gap-x-20">
-                            <button onClick={prevImage} className="text-text-primary">
-                                <ChevronLeft size={60}/>
-                            </button>
-                            <button onClick={nextImage} className="text-text-primary">
-                                <ChevronRight size={60}/>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-            )}
-        </div>
-    );
-}
 
 export function ImagesCollageDesktop({images, buttonText}) {
 
@@ -109,7 +22,7 @@ export function ImagesCollageDesktop({images, buttonText}) {
                 {images.slice(0, visibleCount).map((item, index) => (
                     <div
                         key={item.id}
-                        className={`${item.isVertical ? "col-span-1" : "col-span-2"}`}
+                        className={`cursor-pointer ${item.isVertical ? "col-span-1" : "col-span-2"}`}
                         onClick={() => {
                             setSelectedIndex(index);
                             setIsCarouselOpen(true);
--- src/app/kostymy/CostumesSection.js
+++ src/app/kostymy/CostumesSection.js
@@ -2,76 +2,76 @@
 
 import {useState} from "react";
 import {Button as ShadCNButton, Button} from "@/components/ui/button";
-import Title from "@/common/Title";
+import GalleryCarousel from "@/common/GalleryCarousel/GalleryCarousel";
 
 const longDresses = [
-    {id: 1, src: "/costumes/longDresses/01.jpg", text: "Dlouhé šaty 01"},
-    {id: 2, src: "/costumes/longDresses/02.jpg", text: "Dlouhé šaty 02"},
-    {id: 3, src: "/costumes/longDresses/03.jpg", text: "Dlouhé šaty 03"},
-    {id: 4, src: "/costumes/longDresses/04.jpg", text: "Dlouhé šaty 04"},
-    {id: 5, src: "/costumes/longDresses/05.jpg", text: "Dlouhé šaty 05"},
-    {id: 6, src: "/costumes/longDresses/06.jpg", text: "Dlouhé šaty 06"},
-    {id: 7, src: "/costumes/longDresses/07.jpg", text: "Dlouhé šaty 07"},
-    {id: 8, src: "/costumes/longDresses/08.jpg", text: "Dlouhé šaty 08"},
+    {id: 1, image: "/costumes/longDresses/01.jpg", text: "Dlouhé šaty 01"},
+    {id: 2, image: "/costumes/longDresses/02.jpg", text: "Dlouhé šaty 02"},
+    {id: 3, image: "/costumes/longDresses/03.jpg", text: "Dlouhé šaty 03"},
+    {id: 4, image: "/costumes/longDresses/04.jpg", text: "Dlouhé šaty 04"},
+    {id: 5, image: "/costumes/longDresses/05.jpg", text: "Dlouhé šaty 05"},
+    {id: 6, image: "/costumes/longDresses/06.jpg", text: "Dlouhé šaty 06"},
+    {id: 7, image: "/costumes/longDresses/07.jpg", text: "Dlouhé šaty 07"},
+    {id: 8, image: "/costumes/longDresses/08.jpg", text: "Dlouhé šaty 08"},
 ]
 const shortDresses = [
-    {id: 1, src: "/costumes/shortDresses/01.jpg", text: "Krátké šaty 01"},
-    {id: 2, src: "/costumes/shortDresses/02.jpg", text: "Krátké šaty 02"},
-    {id: 3, src: "/costumes/shortDresses/03.jpg", text: "Krátké šaty 03"},
-    {id: 4, src: "/costumes/shortDresses/04.jpg", text: "Krátké šaty 04"},
-    {id: 5, src: "/costumes/shortDresses/05.jpg", text: "Krátké šaty 05"},
-    {id: 6, src: "/costumes/shortDresses/06.jpg", text: "Krátké šaty 06"},
-    {id: 7, src: "/costumes/shortDresses/07.jpg", text: "Krátké šaty 07"},
-    {id: 8, src: "/costumes/shortDresses/08.jpg", text: "Krátké šaty 08"},
-    {id: 9, src: "/costumes/shortDresses/09.jpg", text: "Krátké šaty 09"},
-    {id: 10, src: "/costumes/shortDresses/10.jpg", text: "Krátké šaty 10"},
-    {id: 11, src: "/costumes/shortDresses/11.jpg", text: "Krátké šaty 11"},
-    {id: 12, src: "/costumes/shortDresses/12.jpg", text: "Krátké šaty 12"},
-    {id: 13, src: "/costumes/shortDresses/13.jpg", text: "Krátké šaty 13"},
-    {id: 14, src: "/costumes/shortDresses/14.jpg", text: "Krátké šaty 14"},
-    {id: 15, src: "/costumes/shortDresses/15.jpg", text: "Krátké šaty 15"},
-    {id: 16, src: "/costumes/shortDresses/16.jpg", text: "Krátké šaty 16"},
-    {id: 17, src: "/costumes/shortDresses/17.jpg", text: "Krátké šaty 17"},
-    {id: 18, src: "/costumes/shortDresses/18.jpg", text: "Krátké šaty 18"},
-    {id: 19, src: "/costumes/shortDresses/19.jpg", text: "Krátké šaty 19"},
-    {id: 20, src: "/costumes/shortDresses/20.jpg", text: "Krátké šaty 20"},
+    {id: 1, image: "/costumes/shortDresses/01.jpg", text: "Krátké šaty 01"},
+    {id: 2, image: "/costumes/shortDresses/02.jpg", text: "Krátké šaty 02"},
+    {id: 3, image: "/costumes/shortDresses/03.jpg", text: "Krátké šaty 03"},
+    {id: 4, image: "/costumes/shortDresses/04.jpg", text: "Krátké šaty 04"},
+    {id: 5, image: "/costumes/shortDresses/05.jpg", text: "Krátké šaty 05"},
+    {id: 6, image: "/costumes/shortDresses/06.jpg", text: "Krátké šaty 06"},
+    {id: 7, image: "/costumes/shortDresses/07.jpg", text: "Krátké šaty 07"},
+    {id: 8, image: "/costumes/shortDresses/08.jpg", text: "Krátké šaty 08"},
+    {id: 9, image: "/costumes/shortDresses/09.jpg", text: "Krátké šaty 09"},
+    {id: 10, image: "/costumes/shortDresses/10.jpg", text: "Krátké šaty 10"},
+    {id: 11, image: "/costumes/shortDresses/11.jpg", text: "Krátké šaty 11"},
+    {id: 12, image: "/costumes/shortDresses/12.jpg", text: "Krátké šaty 12"},
+    {id: 13, image: "/costumes/shortDresses/13.jpg", text: "Krátké šaty 13"},
+    {id: 14, image: "/costumes/shortDresses/14.jpg", text: "Krátké šaty 14"},
+    {id: 15, image: "/costumes/shortDresses/15.jpg", text: "Krátké šaty 15"},
+    {id: 16, image: "/costumes/shortDresses/16.jpg", text: "Krátké šaty 16"},
+    {id: 17, image: "/costumes/shortDresses/17.jpg", text: "Krátké šaty 17"},
+    {id: 18, image: "/costumes/shortDresses/18.jpg", text: "Krátké šaty 18"},
+    {id: 19, image: "/costumes/shortDresses/19.jpg", text: "Krátké šaty 19"},
+    {id: 20, image: "/costumes/shortDresses/20.jpg", text: "Krátké šaty 20"},
 ];
 const coatsAndJackets = [
-    {id: 1, src: "/costumes/coatsAndJackets/01.jpg", text: "Kabáty a saka 01"},
-    {id: 2, src: "/costumes/coatsAndJackets/02.jpg", text: "Kabáty a saka 02"},
-    {id: 3, src: "/costumes/coatsAndJackets/03.jpg", text: "Kabáty a saka 03"},
-    {id: 4, src: "/costumes/coatsAndJackets/04.jpg", text: "Kabáty a saka 04"},
-    {id: 5, src: "/costumes/coatsAndJackets/05.jpg", text: "Kabáty a saka 05"},
-    {id: 6, src: "/costumes/coatsAndJackets/06.jpg", text: "Kabáty a saka 06"},
-    {id: 7, src: "/costumes/coatsAndJackets/07.jpg", text: "Kabáty a saka 07"},
-    {id: 8, src: "/costumes/coatsAndJackets/08.jpg", text: "Kabáty a saka 08"},
-    {id: 9, src: "/costumes/coatsAndJackets/09.jpg", text: "Kabáty a saka 09"},
-    {id: 10, src: "/costumes/coatsAndJackets/10.jpg", text: "Kabáty a saka 10"},
-    {id: 11, src: "/costumes/coatsAndJackets/11.jpg", text: "Kabáty a saka 11"},
-    {id: 12, src: "/costumes/coatsAndJackets/12.jpg", text: "Kabáty a saka 12"},
+    {id: 1, image: "/costumes/coatsAndJackets/01.jpg", text: "Kabáty a saka 01"},
+    {id: 2, image: "/costumes/coatsAndJackets/02.jpg", text: "Kabáty a saka 02"},
+    {id: 3, image: "/costumes/coatsAndJackets/03.jpg", text: "Kabáty a saka 03"},
+    {id: 4, image: "/costumes/coatsAndJackets/04.jpg", text: "Kabáty a saka 04"},
+    {id: 5, image: "/costumes/coatsAndJackets/05.jpg", text: "Kabáty a saka 05"},
+    {id: 6, image: "/costumes/coatsAndJackets/06.jpg", text: "Kabáty a saka 06"},
+    {id: 7, image: "/costumes/coatsAndJackets/07.jpg", text: "Kabáty a saka 07"},
+    {id: 8, image: "/costumes/coatsAndJackets/08.jpg", text: "Kabáty a saka 08"},
+    {id: 9, image: "/costumes/coatsAndJackets/09.jpg", text: "Kabáty a saka 09"},
+    {id: 10, image: "/costumes/coatsAndJackets/10.jpg", text: "Kabáty a saka 10"},
+    {id: 11, image: "/costumes/coatsAndJackets/11.jpg", text: "Kabáty a saka 11"},
+    {id: 12, image: "/costumes/coatsAndJackets/12.jpg", text: "Kabáty a saka 12"},
 ]
 const scraves = [
-    {id: 1, src: "/costumes/scraves/01.jpg", text: "Šátky 01"},
-    {id: 2, src: "/costumes/scraves/02.jpg", text: "Šátky 02"},
-    {id: 3, src: "/costumes/scraves/03.jpg", text: "Šátky 03"},
-    {id: 4, src: "/costumes/scraves/04.jpg", text: "Šátky 04"},
-    {id: 5, src: "/costumes/scraves/05.jpg", text: "Šátky 05"},
-    {id: 6, src: "/costumes/scraves/06.jpg", text: "Šátky 06"},
-    {id: 7, src: "/costumes/scraves/07.jpg", text: "Šátky 07"},
-    {id: 8, src: "/costumes/scraves/08.jpg", text: "Šátky 08"},
+    {id: 1, image: "/costumes/scraves/01.jpg", text: "Šátky 01"},
+    {id: 2, image: "/costumes/scraves/02.jpg", text: "Šátky 02"},
+    {id: 3, image: "/costumes/scraves/03.jpg", text: "Šátky 03"},
+    {id: 4, image: "/costumes/scraves/04.jpg", text: "Šátky 04"},
+    {id: 5, image: "/costumes/scraves/05.jpg", text: "Šátky 05"},
+    {id: 6, image: "/costumes/scraves/06.jpg", text: "Šátky 06"},
+    {id: 7, image: "/costumes/scraves/07.jpg", text: "Šátky 07"},
+    {id: 8, image: "/costumes/scraves/08.jpg", text: "Šátky 08"},
 ]
 const other = [
-    {id: 1, src: "/costumes/other/01.jpg", text: "Ostatní 01"},
-    {id: 2, src: "/costumes/other/02.jpg", text: "Ostatní 02"},
-    {id: 3, src: "/costumes/other/03.jpg", text: "Ostatní 03"},
-    {id: 4, src: "/costumes/other/04.jpg", text: "Ostatní 04"},
-    {id: 5, src: "/costumes/other/05.jpg", text: "Ostatní 05"},
-    {id: 6, src: "/costumes/other/06.jpg", text: "Ostatní 06"},
-    {id: 7, src: "/costumes/other/07.jpg", text: "Ostatní 07"},
-    {id: 8, src: "/costumes/other/08.jpg", text: "Ostatní 08"},
-    {id: 9, src: "/costumes/other/09.jpg", text: "Ostatní 09"},
-    {id: 10, src: "/costumes/other/10.jpg", text: "Ostatní 10"},
-    {id: 11, src: "/costumes/other/11.jpg", text: "Ostatní 11"},
+    {id: 1, image: "/costumes/other/01.jpg", text: "Ostatní 01"},
+    {id: 2, image: "/costumes/other/02.jpg", text: "Ostatní 02"},
+    {id: 3, image: "/costumes/other/03.jpg", text: "Ostatní 03"},
+    {id: 4, image: "/costumes/other/04.jpg", text: "Ostatní 04"},
+    {id: 5, image: "/costumes/other/05.jpg", text: "Ostatní 05"},
+    {id: 6, image: "/costumes/other/06.jpg", text: "Ostatní 06"},
+    {id: 7, image: "/costumes/other/07.jpg", text: "Ostatní 07"},
+    {id: 8, image: "/costumes/other/08.jpg", text: "Ostatní 08"},
+    {id: 9, image: "/costumes/other/09.jpg", text: "Ostatní 09"},
+    {id: 10, image: "/costumes/other/10.jpg", text: "Ostatní 10"},
+    {id: 11, image: "/costumes/other/11.jpg", text: "Ostatní 11"},
 ]
 
 const controlButtons = [
@@ -91,6 +91,9 @@ export default function CostumesSection() {
     const loadMoreImages = () => {
         setVisibleCount(prevCount => prevCount + 4);
     };
+
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [isCarouselOpen, setIsCarouselOpen] = useState(false);
 
     return (
         <div className="mt-10 mb-10">
@@ -114,9 +117,22 @@ export default function CostumesSection() {
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 xl:gap-10">
                 {
                     controlButtons[category].items.slice(0, visibleCount).map((item, index) => (
-                        <div key={index}>
-                            <div className="w-full max-h-[250px] xl:max-h-[400px] overflow-hidden relative object-cover">
-                                <img loading="lazy" className="rounded-[2vh]" src={item.src} alt={item.text}/>
+                        <div
+                            className="cursor-pointer"
+                            key={index}
+                            onClick={() => {
+                                setSelectedIndex(index);
+                                setIsCarouselOpen(true);
+                            }}
+                        >
+                            <div
+                                className="w-full max-h-[250px] xl:max-h-[400px] overflow-hidden relative object-cover">
+                                <img
+                                    loading="lazy"
+                                    className="rounded-[2vh]"
+                                    src={item.image}
+                                    alt={item.text}
+                                />
                             </div>
                             <div className="p-4 rounded-bl-[2vh] rounded-br-[2vh] bg-background-secondary">
                                 <div className="w-2/3 lg:w-1/2 flex flex-row items-center justify-between">
@@ -136,7 +152,8 @@ export default function CostumesSection() {
                         className="min-w-fit group relative transition transform bg-button-button-1 duration-200 ease-in-out p-3 lg:p-6 overflow-hidden"
                         style={{borderRadius: "9999px", position: "relative"}}
                     >
-                        <span className="absolute inset-0 bg-button-button-2 w-0 transition-all duration-300 ease-in-out group-hover:w-full"/>
+                        <span
+                            className="absolute inset-0 bg-button-button-2 w-0 transition-all duration-300 ease-in-out group-hover:w-full"/>
                         <span
                             className="inline-flex w-full h-full justify-center text-text-primary group-hover:text-text-secondary items-center font-sm relative z-10">
                             {"Načíst další kostýmy"}
@@ -144,6 +161,16 @@ export default function CostumesSection() {
                     </ShadCNButton>
                 </div>
             )}
+
+            <div className="flex justify-center mt-4">
+                <GalleryCarousel
+                    images={controlButtons[category].items}
+                    buttonText={""}
+                    initialIndex={selectedIndex}
+                    isCarouselOpen={isCarouselOpen}
+                    setIsCarouselOpen={setIsCarouselOpen}
+                />
+            </div>
         </div>
     );
 }
