284031958eb1b1858aa7b968556c222c2231ac36
Kirill Shibanov <shibakir@fit.cvut.cz>
2025-02-21 20:30:51 +0100

    add links ints facebook

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,6 +1,6 @@
 
 
-export const instagramLink = "https://www.instagram.com";
-export const facebookLink = "https://www.facebook.com";
+export const instagramLink = "https://www.instagram.com/lifestore.cz?igsh=MXVtbDl0aXhjZjNidg==";
+export const facebookLink = "https://www.facebook.com/share/15G22rCaSq/?mibextid=wwXIfr";
 export const linkedinLink = "https://www.linkedin.com";
 export const tiktokLink = "https://www.tiktok.com";
