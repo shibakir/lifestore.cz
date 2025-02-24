e82d8c555f605439a0f97c865f3e585075b15197
Kirill Shibanov <shibakir@fit.cvut.cz>
2025-02-24 11:05:32 +0100

    set header logo to center

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ export default function Header() {
     return (
         <header className="bg-background-secondary sticky top-0 w-full z-50 shadow-md min-h-[70px] flex items-center">
             <div className="main flex row pt-4 pb-4">
-                <div className="hidden 2xl:flex w-6/12 justify-start items-center">
+                <div className="hidden 2xl:flex basis-5/12 justify-start items-center">
                     <nav className="space-x-1 2xl:space-x-2">
                         <NavLink
                             key={1}
@@ -66,10 +66,10 @@ export default function Header() {
                         </NavLink>
                     </nav>
                 </div>
-                <div className="w-5/12 2xl:hidden flex">
+                <div className="basis-5/12 2xl:hidden flex">
                     <MobileMenu pathname={pathname}/>
                 </div>
-                <div className="w-2/12 2xl:w-1/12 flex justify-center items-center">
+                <div className="basis-2/12 flex justify-center items-center">
                     <Link href="/">
                         <Image
                             src="website/lifestore-favicon.svg"
@@ -80,7 +80,7 @@ export default function Header() {
                         />
                     </Link>
                 </div>
-                <div className="w-5/12 space-x-5 flex items-center justify-end">
+                <div className="basis-5/12 space-x-5 flex items-center justify-end">
                     <span id={1}>
                         <a href={instagramLink} target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faInstagram}
